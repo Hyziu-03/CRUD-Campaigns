03db1939f19fa231d9b698beb578cde4c06fe2df
Hyziu-03 <szymon.hyziak@protonmail.com>
2026-06-14 20:42:57 +0200

    chore: optimize for SEO

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,7 @@ import './styles/Global/App.scss'
 function App() {
   return (
     <main className="app-shell">
+      <meta name="description" content="A simple CRUD app for managing ad campaigns" />
       <ProductCampaignList />
     </main>
   )
