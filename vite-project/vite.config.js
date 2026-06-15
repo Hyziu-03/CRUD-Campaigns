02084b7ce699df4eafa9cc77e54548f9606193be
Hyziu-03 <szymon.hyziak@protonmail.com>
2026-06-15 10:37:16 +0200

    chore: reduce chunk size

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,33 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  output: {
+    codeSplitting: {
+      minSize: 20000,
+      groups: [
+        {
+          name: "vendor",
+          test: /node_modules/,
+        },
+      ],
+    },
+  },  
+});
